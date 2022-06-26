0fea088025cd2d6f33b468061835f2e712f013a9
Fidifis <fidifis@seznam.cz>
2022-06-26 21:05:05 +0200

    Add button functions

--- TextureMerge/MainWindow.xaml.cs
+++ TextureMerge/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TextureMerge
 {
@@ -23,6 +24,63 @@ namespace TextureMerge
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string GetImagePath()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select an image file";
+            openFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*"; //TODO: Add more formats
+            if (openFileDialog.ShowDialog() == true)
+                return openFileDialog.FileName;
+            return string.Empty;
+        }
+
+        private void ButtonLoadR(object sender, RoutedEventArgs e)
+        {
+            string path = GetImagePath();
+        }
+
+        private void ButtonLoadG(object sender, RoutedEventArgs e)
+        {
+            string path = GetImagePath();
+        }
+
+        private void ButtonLoadB(object sender, RoutedEventArgs e)
+        {
+            string path = GetImagePath();
+        }
+
+        private void ButtonClearR(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClearG(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClearB(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonBrowse(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Select an image file";
+            saveFileDialog.Filter = "PNG (*.png)|*.png|All files (*.*)|*.*"; //TODO: Add more formats
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                PathToSave.Text = Path.GetDirectoryName(saveFileDialog.FileName);
+                SaveImageName.Text = Path.GetFileName(saveFileDialog.FileName);
+            }
+        }
+
+        private void ButtonMerge(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
