ec016606ffa12ce0d51e0f67c07b8175fbf0cf0d
Fidifis <fidifis@seznam.cz>
2022-06-27 15:15:08 +0200

    Add convert extension

---
+++ TextureMerge/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using SkiaSharp;
+
+namespace TextureMerge
+{
+    public static class Extensions
+    {
+        public static ImageSource? toImageSource(this SKBitmap bitmap)
+        {
+            SKImage image = SKImage.FromPixels(bitmap.PeekPixels());
+            SKData encoded = image.Encode();
+            Stream stream = encoded.AsStream();
+            return new ImageSourceConverter().ConvertFrom(stream) as ImageSource;
+        }
+    }
+}
