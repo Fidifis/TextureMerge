d29e68eec33b01fc181c1950684a09cf1c3cbf1f
Fidifis <fidifis@seznam.cz>
2022-07-17 12:09:17 +0200

    Add checkForUpdate

---
+++ TextureMerge/UpdateCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Reflection;
+using System.Windows;
+
+namespace TextureMerge
+{
+    internal class UpdateCheck
+    {
+        public static void CheckForUpdate()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = "v" + fvi.FileVersion;
+            string content;
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.UserAgent, "TextureMerge_webclient");
+                content = client.DownloadString("https://api.github.com/repos/Fidifis/TextureMerge/releases/latest");
+            }
+
+            string latestVersion = GetValue(content, "tag_name");
+            if (version != latestVersion)
+            {
+                if (MessageBox.Show("New version available.\n" + latestVersion +
+                    "\nDo you want to download it?", "Update", MessageBoxButton.YesNoCancel) == MessageBoxResult.Yes)
+                {
+                    Process.Start("https://github.com/Fidifis/TextureMerge/releases/latest");
+                }
+            }
+        }
+
+        private static string GetValue(string content, string key)
+        {
+            int start, end;
+            start = content.IndexOf(key, 0);
+            end = content.IndexOf(",", start);
+            return content.Substring(start, end - start).Split(':')[1].Trim().Replace("\"", "");
+        }
+    }
+}
