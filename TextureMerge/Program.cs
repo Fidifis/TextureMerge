35b95970f6a24a91978b68d838faee8a03696236
Fidifis <fidifis@seznam.cz>
2022-07-21 19:44:39 +0200

    Add custom entry point

---
+++ TextureMerge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TextureMerge
+{
+    public class Program
+    {
+        [STAThread()]
+        public static void Main(string[] args)
+        {
+            App.Main();
+        }
+    }
+}
