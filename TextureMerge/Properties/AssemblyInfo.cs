37e6e038fb311cb66dfeccc00bcb39bf9c1ed967
Fidifis <fidifis@seznam.cz>
2022-12-15 13:00:55 +0100

    Increase version

--- TextureMerge/Properties/AssemblyInfo.cs
+++ TextureMerge/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
 // pomocí zástupného znaku * takto:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.2")]
-[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.3")]
